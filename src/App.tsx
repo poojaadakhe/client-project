499168d5685272ca8c2ed5cec70b94b50da5c236
Pooja Adakhe <pooja.adakhe@amla.io>
2020-01-07 11:14:44 +0530

    react npm packages use examples

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+
+import { Button,Dropdown, List, Radio, Icon } from '@artifi/common-ui';
+import { DemoData } from './demodata';
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row"><br></br>
+          <div className="col-md-1"><Icon className="glyphicon glyphicon-star"></Icon></div>
+          <div className="col-md-1"><Icon className="glyphicon glyphicon-envelope"></Icon></div>
+          <div className="col-md-1"><Icon className="glyphicon glyphicon-glass"></Icon></div>
+        </div>
+        <div className="row"><br></br>
+          <div className="col-md-3">
+            <Button label="Add Text" style={{ color: '#ffffff' }} ></Button>
+            {/* classes ={"btn btn-warning"} */}
+          </div>
+          <div className="col-md-3">
+            <Dropdown btnClassName={"btn btn-success dropdown-toggle dropdown-toggle-split"}></Dropdown> 
+          </div>
+          <div className="col-md-3">
+            <List demoData={DemoData} listClass="list-group-item"></List>
+          </div>
+          <div className="col-md-3">
+            <Radio></Radio>
+          </div>
+        </div>
+      </div>
+        
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/demodata.tsx
@@ -0,0 +1,22 @@
+export const DemoData = [
+    {
+        id: 1,
+        name: 'Select Value  1'
+    },
+    {
+        id: 2,
+        name: 'Select Value  2'
+    },
+    {
+        id: 3,
+        name: 'Select Value  3'
+    },
+    {
+        id: 4,
+        name: 'Select Value  4'
+    },
+    {
+        id: 5,
+        name: 'Select Value  5'
+    }
+];
